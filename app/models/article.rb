42c905831dbdb532b1988a067d5216460cf5302e
Paul Huisman <paulhuisman88@gmail.com>
2015-05-06 11:38:58 +0200

    Style changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
   validates :title, presence: true,
                     length: { minimum: 5 }
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :playground => "269x200#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :playground_overiew => "269x200#", :playground_wide  => "898x200#"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-end
+end	
