40975083366ff5de20533b8252aa47eef83ce28e
Paul Huisman <paulhuisman88@gmail.com>
2015-05-07 09:21:58 +0200

    logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
   end
 
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
